c4d07fdbea573fa15a8369056078656339d7045a
Arjun <askalburgi@gmail.com>
2025-07-24 15:23:58 -0400

    fixing base path for gh actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const isDev = import.meta.env.MODE === 'development';
+
 export default defineConfig({
   plugins: [react()],
-  base: "/calendarComponent/",
+  base: !isDev ? '/calendarComponent/' : '/',
 })
