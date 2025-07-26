b59c8300621140a93f075ae4f3114562fcc7f1b2
Arjun <askalburgi@gmail.com>
2025-07-26 10:02:50 -0400

    bugfix prod base

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const isDev = process.env.NODE_ENV === 'development';
+export default defineConfig(({ mode }) => {
+  console.log("Vite mode:", mode); 
 
-export default defineConfig({
-  plugins: [react()],
-  base: !isDev ? '/calendarComponent/' : '/',
-})
+  return {
+    plugins: [react()],
+    base: mode === 'development' ? '/' : '/calendarComponent/',
+  };
+});
